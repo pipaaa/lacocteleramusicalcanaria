4989123fe0b2005fd0c8043e98828f0d3ce15a92
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-10-10 21:07:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Espera 2 segundos y luego oculta el elemento de carga
+setTimeout(() => {
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'none';
+}, 2000); // 2000 milisegundos = 2 segundos
