c96e5b2b9cb465f812d5d49633ea77e172e775cb
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-10-10 21:11:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Espera 2 segundos y luego oculta el elemento de carga
+// Oculta todo el contenido al cargar la página
+document.body.classList.add('hide-content');
+
+// Espera 2 segundos y luego muestra el contenido y oculta el símbolo de carga
 setTimeout(() => {
     const loader = document.querySelector('.loader');
     loader.style.display = 'none';
-}, 4000); // 4000 milisegundos = 4 segundos
+    document.body.classList.remove('hide-content');
+}, 2000); // 2000 milisegundos = 2 segundos
